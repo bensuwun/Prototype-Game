45320412762e13a3b205808dc435ee773498d887
symatt <jamesxsy17@gmail.com>
2022-11-08 18:12:51 +0800

    moved WPM calculations to StatsCalc

--- Assets/Scripts/CustomTyper.cs
+++ Assets/Scripts/CustomTyper.cs
@@ -14,12 +14,11 @@ public class CustomTyper : MonoBehaviour
     public TextMeshProUGUI currWPM;
 
     public WordBank wordBank = null;
+    public StatsCalc statsCalc = null;
 
     private string sourceString = string.Empty;
     private List<Word> wordList = new List<Word>();
     private StringBuilder sb;
-
-
 
     // Indexing current word and current char
     private int wordIndex = 0;
@@ -31,8 +30,6 @@ public class CustomTyper : MonoBehaviour
     private string incorrectColor = "red";
     private string defaultColor = "#808080";
 
-    private DateTime start;
-    private DateTime end;
     private int numCharsTyped = 0;
     private int numCorrectChars = 0;
     private int numSpace = 0;
@@ -51,7 +48,7 @@ public class CustomTyper : MonoBehaviour
         SetCurrentWords();
 
         // gets the current time
-        getStart();
+        statsCalc.getStart();
 
         // show current WPM to 0
         currWPM.text = "0";
@@ -108,10 +105,9 @@ public class CustomTyper : MonoBehaviour
                 ResetIndeces();
             }
 
-            getEnd();
             // Display current WPM on screen
-            currWPM.text = getCurrWPM();
-            // currWPM.text = getRawWPM();
+            currWPM.text = statsCalc.getCurrWPM(numCorrectChars, numSpace);
+            // currWPM.text = getRawWPM(numCharsTyped, numSpace);
         } 
     }
 
@@ -271,31 +267,6 @@ public class CustomTyper : MonoBehaviour
 
     }
 
-    // gets the start time
-    public void getStart() {
-        start = DateTime.Now;
-    }
-
-    // gets the end time
-    public void getEnd() {
-        end = DateTime.Now;
-    }
-
-    // calculates the current WPM
-    public string getCurrWPM() {
-        TimeSpan currDiff = end - start;
-        int diffSec = (int) currDiff.TotalSeconds;
-        int currWPM = (int) ((numCorrectChars + numSpace) * (60 / diffSec) / 5);
-        return("" + currWPM);
-    } 
-
-    // calculates raw WPM (including mistakes)
-    public string getRawWPM() {
-        TimeSpan currDiff = end - start;
-        int diffSec = (int) currDiff.TotalSeconds;
-        int rawWPM = (int) ((numCharsTyped + numSpace) * (60 / diffSec) / 5);
-        return("" + rawWPM);
-    } 
     // TODO: Check whether or not the remaining words are 0
     // Checks if the current line is already finished, return true if done, false if not
     private bool AreWordsComplete() {
---
+++ Assets/Scripts/StatsCalc.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+public class StatsCalc : MonoBehaviour
+{
+    public float start;
+    public float end;
+    
+    // gets the start time
+    public void getStart() {
+        start = Time.time;
+    }
+    // gets the end time
+    public void getEnd() {
+        end = Time.time;
+    }
+    // calculates the current WPM
+    public string getCurrWPM(int numCorrectChars, int numSpace) {
+        getEnd();
+        float currDiff = end - start;
+        // removed numspace in the computation
+        // double currWPM = Math.Floor(((numCorrectChars + numSpace) * (60 / currDiff) / 5));
+        double currWPM = Math.Floor(((numCorrectChars) * (60 / currDiff) / 5));
+        return("" + currWPM);
+    } 
+    // calculates raw WPM (including mistakes)
+    public string getRawWPM(int numCharsTyped, int numSpace) {
+        float currDiff = end - start;
+        double rawWPM = Math.Floor(((numCharsTyped + numSpace) * (60 / currDiff) / 5));
+        return("" + rawWPM);
+    } 
+}
