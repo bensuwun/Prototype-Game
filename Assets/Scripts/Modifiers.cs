0810786ddc88fe46bb2fff51e9fa22ee25c937b6
Ivan Lim <ivalim@ymail.com>
2022-11-22 23:06:45 +0800

    added inventory and clear debuffs

--- Assets/Scripts/Modifiers.cs
+++ Assets/Scripts/Modifiers.cs
@@ -7,6 +7,7 @@ public class Modifiers : MonoBehaviour
     public Player player = null;
     public TextMeshProUGUI wpm = null;
     public CustomTyper typer = null;
+    public PlayerInventory inventory = null;
     // Update is called once per frame
     void Update()
     {
@@ -22,10 +23,11 @@ public class Modifiers : MonoBehaviour
     }
 
     void healthRegen(){
-        if(true){ // check if player has hp regen buff
+        if(inventory.hpRegenFlag){ // check if player has hp regen buff
             // int n_wpm = Mathf.FloorToInt((float)0.75 * CombineNumber(wpm.GetParsedText().ToIntArray()));
             // int hpRegen = n_wpm > 20 ? 20 : n_wpm;
             player.increaseHealth(15 );
+            inventory.hpRegenFlag = false;
         }
     }
 
@@ -33,7 +35,7 @@ public class Modifiers : MonoBehaviour
         var words = typer.getWordList(2);
         Word newWord = null;
         List<Word> newWords = new List<Word>();
-        if(true){ // check inventory condition
+        if(inventory.buttonMashFlag){ // check inventory condition
             foreach (var word in words)
             {
                 newWord = createMashWord("a",word.Text.Length);
@@ -41,6 +43,7 @@ public class Modifiers : MonoBehaviour
             }
             typer.SetWordListWords(newWords,2);
             // Debug.Log(string.Format("Len: {0}", newWords.Count));
+            inventory.buttonMashFlag = false;
         }
 
     }
@@ -53,15 +56,9 @@ public class Modifiers : MonoBehaviour
         return new Word(mashWord);
     }
 
-    int CombineNumber(int[] arr){
-        int accum = 0;
-        int i = 0;
-        foreach (var num in arr)
-        {
-            accum += (num*(arr.Length-i));
-            i++;
-        }
-
-        return accum;
+    void clearDebuffs(){
+        inventory.shortSightedFlag = false;
+        inventory.armsSpaghettiFlag = false;
+        inventory.longWordsFlag = false;
     }
 }
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public bool hpRegenFlag { get; set;}
+    public bool buttonMashFlag { get; set;}
+    public bool shortSightedFlag { get; set;}
+    public bool longWordsFlag { get; set;}
+    public bool armsSpaghettiFlag { get; set;}
+
+}
